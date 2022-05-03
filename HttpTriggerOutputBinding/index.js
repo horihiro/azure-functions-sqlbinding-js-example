ae77b390abeb717513a254d987d23c91e2c326e0
horihiro <hori.kawa.shima.tool+github@gmail.com>
2022-05-03 09:17:30 +0900

    reflect https://github.com/Azure/azure-functions-sql-extension/issues/232

--- HttpTriggerOutputBinding/index.js
+++ HttpTriggerOutputBinding/index.js
@@ -2,11 +2,11 @@
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.bindings.employees = JSON.stringify([{
-      "EmployeeId": 1,
-      "FirstName": "Hello",
-      "LastName": "World",
-      "Company": "Microsoft",
-      "Team": "Functions"
+      "employeeid": 1,
+      "firstname": "Hello",
+      "lastname": "World",
+      "company": "Microsoft",
+      "team": "Functions"
     }])
 
     context.res = {
